7fcc415bd0568fe52cd563cf40aeaf5e08408315
Nikunj Chawla <nikchawla312@gmail.com>
2022-02-17 09:11:35 -0600

    Removed commented-out code in run.sh (already in VCS history)

--- src/run.sh
+++ src/run.sh
@@ -1,14 +1,11 @@
 #!/bin/zsh
 
 # NOTES
-# 
+#
 # bash was not working
 # `arg` goes as output from here
 # `autocomplete` needed to keep ScriptFilter result
-# 
-
-# force set PATH (https://github.com/aviaryan/alfred-clc/issues/1)
-# PATH=$($(echo $SHELL) -ic 'echo $PATH'):/usr/local/bin:$PATH
+#
 
 query=$1
 PATH=$PATH:/usr/local/bin
